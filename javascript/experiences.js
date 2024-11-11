37041d126e19e3d16e0fd48424976d1701db95bb
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2024-11-11 13:47:45 +0100

    Add logo to skill section and configure Experiences with Vue.js

---
+++ javascript/experiences.js
@@ -0,0 +1,52 @@
+import { createApp } from "vue";
+
+console.log("connected from experiences");
+
+createApp({
+  data() {
+    return {
+      experiences: [
+        {
+          name:"Le Wagon, 9-weeks full time intensive coding bootcamp",
+          employer:"Le Wagon",
+          job_title: "9-weeks full time intensive coding bootcamp",
+          job_logo:"images/le_wagon_logo.png",
+          date_start:"06/10/2024",
+          date_end:"05/12/2024",
+          description_short:"9-week full time intensive coding bootcamp. I learned and used HTML, CSS, JavaScript, SQL, Git, GitHUb, Heroku Ruby and Ruby on Rails. With a team of four students, we designed, implemented and shopped to production a clone of AirBn'B in a week. During the last 2 weeks, still as a team we pitched an idea for a mobile webapp, Sportify, and completed the project in 2 weeks",
+          description_long:""
+        },
+        {
+          name:"Le Wagon, 9-weeks full time intensive coding bootcamp",
+          employer:"Le Wagon",
+          job_title: "9-weeks full time intensive coding bootcamp",
+          job_logo:"images/le_wagon_logo.png",
+          date_start:"06/10/2024",
+          date_end:"05/12/2024",
+          description_short:"9-week full time intensive coding bootcamp. I learned and used HTML, CSS, JavaScript, SQL, Git, GitHUb, Heroku Ruby and Ruby on Rails. With a team of four students, we designed, implemented and shopped to production a clone of AirBn'B in a week. During the last 2 weeks, still as a team we pitched an idea for a mobile webapp, Sportify, and completed the project in 2 weeks",
+          description_long:""
+        },
+        {
+          name:"Le Wagon, 9-weeks full time intensive coding bootcamp",
+          employer:"Le Wagon",
+          job_title: "9-weeks full time intensive coding bootcamp",
+          job_logo:"images/le_wagon_logo.png",
+          date_start:"06/10/2024",
+          date_end:"05/12/2024",
+          description_short:"9-week full time intensive coding bootcamp. I learned and used HTML, CSS, JavaScript, SQL, Git, GitHUb, Heroku Ruby and Ruby on Rails. With a team of four students, we designed, implemented and shopped to production a clone of AirBn'B in a week. During the last 2 weeks, still as a team we pitched an idea for a mobile webapp, Sportify, and completed the project in 2 weeks",
+          description_long:""
+        },
+        {
+          name:"Le Wagon, 9-weeks full time intensive coding bootcamp",
+          employer:"Le Wagon",
+          job_title: "9-weeks full time intensive coding bootcamp",
+          job_logo:"images/le_wagon_logo.png",
+          date_start:"06/10/2024",
+          date_end:"05/12/2024",
+          description_short:"9-week full time intensive coding bootcamp. I learned and used HTML, CSS, JavaScript, SQL, Git, GitHUb, Heroku Ruby and Ruby on Rails. With a team of four students, we designed, implemented and shopped to production a clone of AirBn'B in a week. During the last 2 weeks, still as a team we pitched an idea for a mobile webapp, Sportify, and completed the project in 2 weeks",
+          description_long:""
+        }
+      ]
+    };
+  }
+}).mount('#experiences');
--- javascript/skills.js
+++ javascript/skills.js
@@ -1,18 +1,60 @@
 import { createApp } from "vue";
 
+// console.log("connected from skills");
+
 createApp({
   data() {
     return {
       skills: [
-        {name: "Github", icon_path:"./images/download.svg", description:"Use of github to track down changes during my projects"},
-        {name: "Test", icon_path:"./images/download.svg", description:"Use of github to track down changes during my projects"}
-      ],
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        },
+        {
+          name: "Github",
+          icon_svg:"images/svg_icons/github.svg"
+        }
+      ]
     };
-  },
-  methods: {
-    displayTodo() {
-      console.log("test vue JS");
-    }
   }
-
 }).mount('#skills');
